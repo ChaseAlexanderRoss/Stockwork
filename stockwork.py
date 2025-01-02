bb935c5d164d106bd94512e7c7db09df326919c5
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2025-01-02 13:51:27 -0500

    1/2/25

--- stockwork.py
+++ stockwork.py
@@ -54,7 +54,7 @@ def generate_stock_summary(stock_name):
                 messages=[
                     {"role": "user", "content": prompt}
                 ],
-                max_completion_tokens=300,
+                max_completion_tokens=5000,
                 temperature=0.7
             )
 
